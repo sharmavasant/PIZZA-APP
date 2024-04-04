6b40d31091529462cd5c01a2b24dc7f20f27ec48
VASANT KUMAR SHARMA <sharmavasant476@gmail.com>
2024-04-04 11:01:59 +0530

    components created

---
+++ pizza-app/src/features/menu/Menu.jsx
@@ -0,0 +1,21 @@
+import { useLoaderData } from "react-router-dom";
+import { getMenu } from "../../services/apiRestaurant";
+import MenuItem from "../menu/MenuItem";
+
+function Menu() {
+  const menu = useLoaderData();
+  return (
+    <div>
+      <ul>
+        {menu.map((pizza) => (
+          <MenuItem pizza={pizza} key={pizza.id} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+export async function loader() {
+  const menu = await getMenu();
+  return menu;
+}
+export default Menu;
---
+++ pizza-app/src/ui/AppLayout.jsx
@@ -0,0 +1,20 @@
+import { Outlet, useNavigation } from "react-router-dom";
+import Header from "./Header";
+import CartOverview from "../features/cart/CartOverview";
+import Loader from "./Loader";
+
+function AppLayout() {
+  const loader = useNavigation().state === "loading";
+  return (
+    <div className="layout">
+      {loader && <Loader />}
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+      <CartOverview />
+    </div>
+  );
+}
+
+export default AppLayout;
